a2e8802edeba0d4cc633b60b7d323b6d75e68e06
cayenne-pepper <ju_silva@icloud.com>
2019-12-06 10:57:34 +0100

    added table

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const db = require("./db");
+const Image = require("./image/model");
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
